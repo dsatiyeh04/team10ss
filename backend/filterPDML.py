512c4360dab69b532b1bb6951a0140f8bd620f6a
root <root@localhost.localdomain>
2018-11-28 22:07:19 -0500

    Adding icmp to filter

--- backend/filterPDML.py
+++ backend/filterPDML.py
@@ -2,14 +2,28 @@
 
 import xml.etree.ElementTree as et
 
-tree = et.parse('test.pdml')
+tree = et.parse('ipv4frags.pdml')
 
 root = tree.getroot()
 
+found = 0
+
 for packet in root.iter('packet'):
     for proto in packet.findall('proto'):
-        if proto.get('name') != 'lapd':
+        # if proto.get('name') != 'lapd':
+        # packet.remove(proto)
+
+        if proto.get('name') == 'icmp':
+            for field in proto.findall('field'):
+                if field.get('showname') == 'Type: 8 (Echo (ping) request)':
+                    found = 1
+
+            if found == 0:
+                packet.remove(proto)
+
+            found = 0
+
+        if proto.get('name') != 'icmp':
             packet.remove(proto)
 
-
-tree.write("output.pdml")
+tree.write("output2.pdml")
